97bbf07b3b1b694537a5920e6c3112c61ecc06d4
Peter Lustig <pelu@universal-robots.com>
2023-06-25 19:50:55 +0000

    Add workaround for shunit2 assert bug.

--- test/run-all-tests.sh
+++ test/run-all-tests.sh
@@ -11,6 +11,30 @@
 # for the 'local' keyword.
 #
 
+if ! hash shunit2 2>/dev/null; then
+  echo "ERROR: Need to install 'shunit2' and/or add it to PATH." >&2
+  exit 1
+fi
+
+# Versions of `shunit2` prior to 2.1.8 did not report "FAIL" when
+# one or more assertions failed within a unit test.
+ShunitAssertBug=$(
+  # shellcheck disable=SC2312 # shunit2 existence already verified by `hash`
+  SHUNIT_VERSION=$(grep '^SHUNIT_VERSION=' "$(command -v shunit2)" \
+                   | cut -d= -f2 | tr -d '"'\''"')
+
+  IFS='.'
+  # shellcheck disable=SC2086 # intentional word-splitting
+  set ${SHUNIT_VERSION}
+
+  test "${1:-0}" -gt 2 \
+     || { test "${1:-0}" -eq 2 \
+          && { test "${2:-0}" -gt 1 \
+               || { test "${2:-0}" -eq 1 \
+                    && test "${3:-0}" -ge 8; }; }; }
+  echo $?
+)
+
 if [ $# -gt 0 ]; then
   TestShells="$*"
 else
@@ -32,6 +56,12 @@ for test in ./test-*.sh; do
     echo "${test#*/} ($shell)"
     echo '--------------------------------'
 
-    "$shell" "$test" || exit $?
+    if [ "$ShunitAssertBug" -eq 1 ]; then
+      { "$shell" "$test" \
+        | awk '/FAIL|ASSERT/ { f=1 } { print } END { if (f) {  exit 1 } }'; } \
+      || exit $?
+    else
+      "$shell" "$test" || exit $?
+    fi
   done
 done
