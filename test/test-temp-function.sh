a9fbc143656dc7b46fe154398289f5a202439293
Peter Lustig <pelu@universal-robots.com>
2023-06-27 05:01:59 +0000

    Removed local variable usage from temp-functions.sh

--- test/test-temp-function.sh
+++ test/test-temp-function.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
-# shellcheck disable=SC3043 # allow 'local' usage
 
-# shellcheck disable=SC2164 # chance of `cd` failing is neglible
+# shellcheck disable=SC2164,SC2312 # chance of `cd` failing is neglible
+# shellcheck disable=SC1091 # do not follow source
 . "$(cd -- "$(dirname "$0")"; pwd)/../lib/include-function.sh"
 include ../lib/temp-functions.sh
 
@@ -10,19 +10,19 @@ test_get_temp_file() {
   assertFalse "empty variable name" "get_temp_file ''"
   assertFalse "invalid variable name" "get_temp_file '?'"
 
-  assertFalse 'extra argument' "get_temp_file temp_file_path x"
+  assertFalse 'extra argument' "get_temp_file _test_get_temp_file_file x"
 
-  local temp_file_status
-  local temp_file_path
-  get_temp_file temp_file_path; temp_file_status=$?
-  assertEquals 'file generated' 0 "$temp_file_status"
+  get_temp_file _test_get_temp_file_file; _test_get_temp_file_status=$?
+  assertEquals 'file generated' 0 "$_test_get_temp_file_status"
 
-  assertNotNull 'file name published' "${temp_file_path:-}"
-  assertTrue 'file exists' "[ -r '${temp_file_path:-}' ]"
+  assertNotNull 'file name published' "${_test_get_temp_file_file:-}"
+  assertTrue 'file exists' "[ -r '${_test_get_temp_file_file:-}' ]"
 
   trigger_exit_handlers
 
-  assertFalse 'file cleaned on exit' "[ -r '${temp_file_path:-}' ]"
+  assertFalse 'file cleaned on exit' "[ -r '${_test_get_temp_file_file:-}' ]"
+
+  unset _test_get_temp_file_status _test_get_temp_file_file
 }
 
 test_get_temp_dir() {
@@ -30,19 +30,19 @@ test_get_temp_dir() {
   assertFalse "empty variable name" "get_temp_dir ''"
   assertFalse "invalid variable name" "get_temp_dir '?'"
 
-  assertFalse 'extra argument' "get_temp_dir temp_dir_path x"
+  assertFalse 'extra argument' "get_temp_dir _test_get_temp_dir_dir x"
 
-  local temp_dir_status
-  local temp_dir_path
-  get_temp_dir temp_dir_path; temp_dir_status=$?
-  assertEquals 'dir generated' 0 "$temp_dir_status"
+  get_temp_dir _test_get_temp_dir_dir; _test_get_temp_dir_status=$?
+  assertEquals 'dir generated' 0 "$_test_get_temp_dir_status"
 
-  assertNotNull 'dir name published' "${temp_dir_path:-}"
-  assertTrue 'dir exists' "[ -d '${temp_dir_path:-}' ]"
+  assertNotNull 'dir name published' "${_test_get_temp_dir_dir:-}"
+  assertTrue 'dir exists' "[ -d '${_test_get_temp_dir_dir:-}' ]"
 
   trigger_exit_handlers
 
-  assertFalse 'dir cleaned on exit' "[ -d '${temp_dir_path:-}' ]"
+  assertFalse 'dir cleaned on exit' "[ -d '${_test_get_temp_dir_dir:-}' ]"
+
+  unset _test_get_temp_dir_status _test_get_temp_dir_dir
 }
 
 if [ "${ZSH_VERSION:-}" ]; then
